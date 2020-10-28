804b2db085717aa44ad5aa59ab56e0ff6329d10e
YoussefPasha <jojoyossefbasha010@gmail.com>
2020-10-28 14:02:50 +0200

    restruction project with components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,6 @@
 import React from 'react';
-import jumboData from './fixtures/jumbo.json';
-
-import Jumbotron from './components/jumbotron';
+import { JumbotronContainer } from './containers/jumbotron';
 
 export default function App() {
-  return (
-    <Jumbotron.Container>
-      {jumboData.map((item) => (
-        <Jumbotron key={item.id} direction={item.direction}>
-          <Jumbotron.Pane>
-            <Jumbotron.Title>{item.title} </Jumbotron.Title>
-            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-          </Jumbotron.Pane>
-          <Jumbotron.Pane>
-            <Jumbotron.Image alt={item.alt} src={item.image} />
-          </Jumbotron.Pane>
-        </Jumbotron>
-      ))}
-    </Jumbotron.Container>
-  );
+  return <JumbotronContainer />;
 }
