1f8c468a7bf19d1b36be1a77f490f68a3a4a5f01
Youssef Pasha <jojoyossefbasha010@gmail.com>
2020-11-19 18:21:25 +0200

    cretae filter films and filter series util

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { useContent } from '../hooks';
 
 export default function Browse() {
-  const { series } = useContent('series');
-  console.log(series);
   return (
     <div>
       <h1>Browse</h1>
