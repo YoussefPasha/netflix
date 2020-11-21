5b2ed8baa363b951e8ab5311941a198aa4f9fd64
Youssef Pasha <jojoyossefbasha010@gmail.com>
2020-11-21 18:40:03 +0200

    done adding signout function and action with style

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -100,6 +100,14 @@ export const Group = styled.div`
   align-items: center;
 `;
 
+export const Picture = styled.button`
+  background: url(${({ src }) => src});
+  background-size: contain;
+  border: 0;
+  width: 32px;
+  height: 32px;
+  cursor: pointer;
+`;
 export const Dropdown = styled.div`
   display: none;
   background-color: black;
@@ -108,6 +116,28 @@ export const Dropdown = styled.div`
   width: 100px;
   top: 32px;
   right: 10px;
+
+  ${Group}:last-of-type ${Link} {
+    cursor: pointer;
+  }
+  ${Group} {
+    margin-bottom: 10px;
+    &::last-of-type {
+      margin-bottom: 0;
+    }
+    ${Link},${Picture} {
+      cursor: default;
+    }
+  }
+  button {
+    margin-right: 10px;
+  }
+
+  p {
+    font-size: 12px;
+    margin-bottom: 0;
+    margin-top: 0;
+  }
 `;
 
 export const Profile = styled.div`
@@ -124,13 +154,4 @@ export const Profile = styled.div`
     display: flex;
     flex-direction: column;
   }
-`;
-
-export const Picture = styled.button`
-  background: url(${({ src }) => src});
-  background-size: contain;
-  border: 0;
-  width: 32px;
-  height: 32px;
-  cursor: pointer;
 `;
