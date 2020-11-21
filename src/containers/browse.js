09c666f9e88c656c0c839f1fa94d42534561916e
Youssef Pasha <jojoyossefbasha010@gmail.com>
2020-11-22 00:13:27 +0200

    add categories with maping items

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -5,6 +5,7 @@ import { FirebaseContext } from '../context/firebase';
 import { Loading, Header, Card } from '../components';
 import * as ROUTES from '../constants/routes';
 import logo from './logo.svg';
+import { Item } from '../components/jumbotron/styles/jumbotron';
 
 export function BrowseContainer({ slides }) {
   const [category, setCategory] = useState('series');
@@ -79,7 +80,20 @@ export function BrowseContainer({ slides }) {
           <Header.PlayButton>Play</Header.PlayButton>
         </Header.Feature>
       </Header>
-      <Card.Group />
+      <Card.Group>
+        {slideRows.map((slideItem) => (
+          <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
+            <Card.Title>{slideItem.title}</Card.Title>
+            <Card.Entities>
+              {slideItem.data.map((item) => (
+                <Card.Item key={Item.docId} item={item}>
+                  <Card.Image src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`} />
+                </Card.Item>
+              ))}
+            </Card.Entities>
+          </Card>
+        ))}
+      </Card.Group>
     </>
   ) : (
     <SelectProfileContainer user={user} setProfile={setProfile} />
